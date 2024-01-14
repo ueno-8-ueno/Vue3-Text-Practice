d27b647f276b25d1379ed2fdfc16fdb22a6bc885
uuuu-8 <104149556+ueno-8-ueno@users.noreply.github.com>
2024-01-14 15:14:19 +0900

    [Finish]6.1節終了

--- chapter6/section6_1/src/App.vue
+++ chapter6/section6_1/src/App.vue
@@ -1,47 +1,68 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from 'vue';
+
+// 6.1.1
+const number = ref(80);
+const showOrNot = computed(
+  (): boolean => {
+    let showOrNot = false;
+    const rand = Math.round(Math.random() * 100);
+    if(rand >= 50){
+      showOrNot = true;
+    }
+    return showOrNot;
+  }
+);
+
+// 6.1.2
+const randomNumber = computed(
+  (): number => {
+    return Math.round(Math.random() * 100);
+  }
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!--6.1.1-->
+  <p v-if="number >= 50">
+    表示①
+  </p>
+  <p v-if="Math.round(Math.random() * 100) >= 50">
+    表示②
+  </p>
+  <p v-if="showOrNot">
+    表示③
+  </p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <!--6.1.2-->
+  <p>
+    点数は{{ randomNumber }}点で
+    <span v-if="randomNumber >= 80">優です。</span>
+    <span v-else-if="randomNumber >= 70">良です。</span>
+    <span v-else-if="randomNumber >= 60">可です。</span>
+    <span v-else>不可です。</span>
+  </p>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!--6.1.3-->
+  <p>
+    点数は{{ randomNumber }}点
+    <template v-if="randomNumber >= 80">
+      で優です。
+      <span style="color: red;">素晴らしい！</span>
+    </template>
+  </p>
+
+  <!--6.1.4-->
+  <section>
+    v-ifを利用
+    <p v-if="showOrNot">
+      条件に合致したので表示_6.1.4
+    </p>
+  </section>
+  <section>
+    v-showを利用
+    <p v-show="showOrNot">
+      条件に合致したので表示_6.1.4
+    </p>
+  </section>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
